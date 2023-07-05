e7665c6efc400c9e93d22b6725282d41922fe2fc
DavletGitacc <davletazimov142@gmail.com>
2023-07-05 23:03:15 +0600

    commit1

--- handlers/notiiflication.py
+++ handlers/notiiflication.py
@@ -1,15 +1,7 @@
-# from apscheduler.schedulers.asyncio import AsyncIOScheduler
-# scheduler = AsyncIOScheduler()
-# config.py
-
-from config import bot, #scheduler
+from aiogram import types
+from config import bot, scheduler
 from aiogram.types import Message
-
-# from config import scheduler
-# from handlers.notiflication import handle_scheduler
 # scheduler.start()
-# dp.register_message_handler(handle_scheduler, commands=['sche'])
-# main.py
 
 async def handle_scheduler(message:Message):
     scheduler.add_job(
@@ -17,10 +9,13 @@ async def handle_scheduler(message:Message):
         'cron',
         day_of_week='last mon',
         hour='22',
-        args=(message.from_user.id, )
+        args=(message.from_user.id, (message.text).replace('Нпомни'))
     )
     await message.answer('хорошо напомню')
 
 async def send_notiflication(chat_id:int):
     await bot.send_message(chat_id=chat_id, text='напоминаюю')
 
+async def cancle_notif(message: types.Message):
+    scheduler.remove_job(job_id=message.from_user.id)
+    await message.answer('не буду напоминать')
